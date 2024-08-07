3f7eaf69497bfff142013ce306fea2b507ffdc05
Gyan <gyanprakashdangi@gmail.com>
2024-08-07 12:25:48 +0530

    send mail through node mailer

--- FIle Upload/Models/file.js
+++ FIle Upload/Models/file.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
 	name:{
@@ -17,5 +19,38 @@ const fileSchema = new mongoose.Schema({
 		required:true,
 	}
 })
+
+
+// post middleware:
+fileSchema.post("save",async function(doc){
+	try {
+		console.log("docs",doc);
+
+		// transporter:
+		let trasnporter =nodemailer.createTransport({
+			host:process.env.MAIL_HOST,
+			auth:{
+				user:process.env.MAIL_USER,
+				pass:process.env.MAIL_PASS
+			}
+		});
+
+
+		//send mail:
+		let info = await trasnporter.sendMail({
+			from:`Gyan Mail sender`,
+			to:doc.email,
+			subject:"NEw file uploaded on Cloudinary",
+			html:`<h2>HEllo ji kaise ho</h2>
+					Your file is uploaded to Cloudinary
+					<a href='${doc.imageUrl}'>${doc.imageUrl}</a>`
+		})
+
+
+	} catch (error) {
+		console.log(error);
+	}
+})
+
 
 module.exports = mongoose.model("File",fileSchema);
